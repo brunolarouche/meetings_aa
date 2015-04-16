ba14d7d21ccf5100d1f482022404fe0a9aeb18e7
Bruno Larouche <bruno.larouche@gmail.com>
2015-04-16 12:04:22 -0400

    Fix for youtube video codes in js.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $( ".video-link" ).click(function(event) {
+    var videoLink;
     event.preventDefault();
-    $(this).next('.video-popup').append('<iframe class="youtube-iframe" id="player" type="text/html" width="640" height="390" src="http://www.youtube.com/embed/bVdN954oCX8?enablejsapi=1&origin=http://example.com&autohide=1&autoplay=1&rel=0" frameborder="0"></iframe>')
+
+    videoLink = $(this).data('youtube-id')
+
+    $(this).next('.video-popup').append('<iframe class="youtube-iframe" id="player" type="text/html" width="640" height="390" src="http://www.youtube.com/embed/' + videoLink + '?enablejsapi=1&origin=http://example.com&autohide=1&autoplay=1&rel=0" frameborder="0"></iframe>')
   });
   $(".video-link").leanModal({ closeButton: ".close-classic" });
 
