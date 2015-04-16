8db847173d68c216e860bc90d402e517c1ff7bba
Bruno Larouche <bruno.larouche@gmail.com>
2015-04-16 14:49:56 -0400

    New mobile videos added.

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     videoLink = $(this).data('youtube-id')
 
-    $(this).next('.video-popup').append('<iframe class="youtube-iframe" id="player" type="text/html" width="640" height="390" src="http://www.youtube.com/embed/' + videoLink + '?enablejsapi=1&origin=http://example.com&autohide=1&autoplay=1&rel=0" frameborder="0"></iframe>')
+    $(this).next('.video-popup').append('<iframe class="youtube-iframe" id="player" type="text/html" width="640" height="390" src="http://www.youtube.com/embed/' + videoLink + '?enablejsapi=1&origin=http://meetingsavecaa.com&autohide=1&autoplay=1&rel=0" frameborder="0"></iframe>')
   });
   $(".video-link").leanModal({ closeButton: ".close-classic" });
 
