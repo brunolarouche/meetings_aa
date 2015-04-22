fe71bb06a2c0c9641b81e449b78f6b7b55642b18
Bruno Larouche <bruno.larouche@gmail.com>
2015-04-22 14:31:25 -0400

    Script test for meetings avec aa.

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,19 @@ $(document).ready(function() {
 
     $(this).next('.video-popup').append('<iframe class="youtube-iframe" id="player" type="text/html" width="640" height="390" src="http://www.youtube.com/embed/' + videoLink + '?enablejsapi=1&origin=http://meetingsavecaa.com&autohide=1&autoplay=1&rel=0" frameborder="0"></iframe>')
   });
+
   $(".video-link").leanModal({ closeButton: ".close-classic" });
 
   $('#lean_overlay, .close-classic').click(function(event) {
     event.preventDefault();
     $('.youtube-iframe').remove();
   });
+
+  $(".named-thumb").hover(
+    function() {
+      $(this).next().toggleClass('hide');
+    }, function() {
+      $(this).next().toggleClass('hide');
+    }
+  );
 });
